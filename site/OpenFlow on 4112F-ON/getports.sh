470e93dc253eb7911c32814964a7e3e206def7d7
Grant Curell <grantcurell@gmail.com>
2022-10-25 14:48:58 -0400

    Deployed ec9f76c with MkDocs version: 1.4.1

--- site/OpenFlow on 4112F-ON/getports.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-
-curl -X GET http://localhost:8080/gelante/getports/150013889525632
--- site/OpenFlow on 4112F-ON/redirectport.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-
-curl -X PUT -d '{"operation": "add", "tcp_port" : 80, "out_port" : 1}' http://127.0.0.1:8080/gelante/redirectport/150013889525632
